7e1b129eaf93ccfe89a87c1428858d94791c1d77
Łukasz Otowski <lukaszotowski@interia.pl>
2019-03-24 22:00:51 +0100

    init

---
+++ deep-logic/js/script.js
@@ -0,0 +1,6 @@
+let LangContainer = document.getElementById('language-select');
+let Option = langContainer.getElementsByClassName('language-select-option');
+
+let ContentMinimal
+let ContentGeneral
+let ContentDetail
