2045b8e3cce4e525b5f95c9ab478af58e19932dc
Łukasz Otowski <lukaszotowski@interia.pl>
2019-05-24 23:41:47 +0200

    refresh directory tree

---
+++ src/views/chemistry/Content.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="">
+    <ChemistryNav/>
+    ?
+  </div>
+</template>
+
+<script>
+import ChemistryNav from '@/components/ChemistryNav.vue'
+
+export default {
+  components:{
+    ChemistryNav
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
