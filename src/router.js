47d93b3506d5ff679339b3dfd163aa47ea9cf36f
Łukasz Otowski <lukaszotowski@interia.pl>
2019-05-24 19:23:25 +0200

    pl add programming/ content

--- src/router.js
+++ src/router.js
@@ -17,7 +17,17 @@ export default new Router({
       path:'/chemistry',
       name:'Chemistry',
       component: function () {
-        return import(/* webpackChunkName: "about" */ './views/Chemistry.vue')
+        return import(/* webpackChunkName: "about" */ './views/chemistry/Content.vue')
+      }
+    },
+    {
+      path: '/programming',
+      name: 'Programming',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: function () {
+        return import(/* webpackChunkName: "programming" */ './views/programming/Content.vue')
       }
     },
     {
@@ -27,7 +37,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: function () {
-        return import(/* webpackChunkName: "frontend" */ './views/frontend/Element.vue')
+        return import(/* webpackChunkName: "frontend" */ './views/frontend/Content.vue')
       }
     },
     {
---
+++ src/views/programming/Content.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <CoreFrontEndNav/> -->
+    <section class="container main lang-pl flex">
+        <header class="header">
+            <h2></h2>
+        </header>
+        <p>Język</p>
+        <p>IDE - Integrated Development Environment</p>
+        <p>Framework - advenced work tools</p>
+    </section>
+  </div>
+</template>
+
+<script>
+// import CoreFrontEndNav from '@/components/CoreFrontEndNav.vue'
+
+export default {
+  name: 'Content',
+  components: {
+    // CoreFrontEndNav
+  }
+}
+</script>
